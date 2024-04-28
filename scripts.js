23cfe67d6cc4cc174d68bd2cb76773780c7b9d4a
NaveedSuhail <lmno3378@gmail.com>
2024-04-29 03:20:00 +0530

    DG round 1

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Bootstrap carousel
+var carousel = new bootstrap.Carousel(document.getElementById('carouselExampleSlidesOnly'), {
+    interval: 3000
+});
+
+// testimonial carousel
+var testimonialCarousel = new bootstrap.Carousel(document.getElementById('carouselTestimonials'), {
+    interval: false
+});
+
